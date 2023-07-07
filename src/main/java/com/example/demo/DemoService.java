b4ca0cc9db66d20d6d6b2a4fcafa0cb900f0d921
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2023-07-07 08:23:29 -0300

    add changes

--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -1,18 +1,37 @@
 package com.example.demo;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.messaging.support.GenericMessage;
+
+import reactor.core.publisher.Flux;
 
 @Configuration
 public class DemoService {
     
     @Bean
-    public Consumer<Message<String>> consumer() {
+    public Consumer<Map<String, Object>> consumer() {
         return message -> {
-            System.out.println("Message: " + message.getPayload());
+            System.out.println("Message: " + message);
         };
     }
+
+	int messageId = 0;
+
+    @Bean
+	public Supplier<Flux<Message<String>>> producer() {
+		return () -> Flux.just("test")
+            .map(GenericMessage::new);
+	}
 }
--- src/main/java/com/example/demo/LocalstackConfiguration.java
+++ src/main/java/com/example/demo/LocalstackConfiguration.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
 
@@ -16,21 +19,39 @@ public class LocalstackConfiguration {
     
     private final String endpoint = "http://localhost:4566";
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private  String region;
 
     @Bean
     public DynamoDbAsyncClient dynamoDbAsyncClient() {
         return DynamoDbAsyncClient.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(
+                    "foo", "bar"
+                )
+            ))
             .region(Region.of(region))
             .endpointOverride(URI.create(endpoint))
             .build();
     }
 
     @Bean
-    public KinesisAsyncClient kinesisAsyncClient(AwsCredentialsProvider awsCredentialsProvider) {
+    public KinesisAsyncClient kinesisAsyncClient() {
         return KinesisAsyncClient.builder()
-            .credentialsProvider(awsCredentialsProvider)
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(
+                "foo", "bar"
+            )))
+            .region(Region.of(region))
+            .endpointOverride(URI.create(endpoint))
+            .build();
+    }
+
+    @Bean
+    public CloudWatchAsyncClient cloudWatchAsyncClient() {
+        return CloudWatchAsyncClient.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(
+                "foo", "bar"
+            )))
             .region(Region.of(region))
             .endpointOverride(URI.create(endpoint))
             .build();
